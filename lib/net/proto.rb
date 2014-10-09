fc0308074f3699da2d1b53bcb78e8b93566ec970
Daniel Berger <djberg96@gmail.com>
2014-10-09 13:07:40 -0600

    Require file designed for Windows on Windows.

--- lib/net/proto.rb
+++ lib/net/proto.rb
@@ -1,10 +1,12 @@
 require 'rbconfig'
 
 case RbConfig::CONFIG['host_os']
-when /linux/i
-  require 'linux/net/proto'
-when /sunos|solaris/i
-  require 'sunos/net/proto'
-else
-  require 'generic/net/proto'
+  when /linux/i
+    require 'linux/net/proto'
+  when /sunos|solaris/i
+    require 'sunos/net/proto'
+  when /mingw|cygwin|win32|windows|mswin/i
+    require 'windows/net/proto'
+  else
+    require 'generic/net/proto'
 end
